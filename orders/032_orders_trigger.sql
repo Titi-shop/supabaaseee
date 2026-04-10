090e00c678f5c5888ff1576b1c392004c7ad2ee1
quản lý supabase <226829233+Titi-shop@users.noreply.github.com>
2026-04-11 04:12:03 +0700

    Delete orders/032_orders_trigger.sql

--- orders/032_orders_trigger.sql
+++
@@ -1,6 +0,0 @@
-DROP TRIGGER IF EXISTS trg_orders_updated ON public.orders;
-
-CREATE TRIGGER trg_orders_updated
-BEFORE UPDATE ON public.orders
-FOR EACH ROW
-EXECUTE FUNCTION public.update_updated_at();
