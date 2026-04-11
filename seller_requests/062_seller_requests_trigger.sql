4b98b8e26467cc29cd19169998fa83fb37033659
quản lý supabase <226829233+Titi-shop@users.noreply.github.com>
2026-04-11 13:52:54 +0700

    Delete seller_requests/062_seller_requests_trigger.sql

--- seller_requests/062_seller_requests_trigger.sql
+++
@@ -1,6 +0,0 @@
-DROP TRIGGER IF EXISTS trg_seller_requests_updated ON public.seller_requests;
-
-CREATE TRIGGER trg_seller_requests_updated
-BEFORE UPDATE ON public.seller_requests
-FOR EACH ROW
-EXECUTE FUNCTION public.update_updated_at();
